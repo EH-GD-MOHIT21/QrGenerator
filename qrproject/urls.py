126bd7e7e1de6875a600852a8e219307a6bb5eb4
EH-GD-MOHIT21 <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2021-07-09 14:25:04 +0530

    Update urls.py

--- qrproject/urls.py
+++ qrproject/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('buchad/', admin.site.urls),
     path('',include('main.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT})
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
